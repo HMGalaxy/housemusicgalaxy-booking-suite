053a65b228b2df43354942d1cc98542cdc3fb05b
HMGalaxy <housemusicgalaxy@gmail.com>
2026-07-22 20:08:04 -0400

    Add files via upload

--- shared/js/core/bootstrap.js
+++ shared/js/core/bootstrap.js
@@ -2,8 +2,8 @@ import {createEventBus} from './event-bus.js';
 import {createStore} from './state.js';
 import {storage} from './storage.js';
 
-export const VERSION='10.3.0';
-export const RELEASE='Connected Workflow Runtime';
+export const VERSION='10.3.1';
+export const RELEASE='Forms Interaction QA Hotfix';
 export const BUILD='2026-07-22T23:30:00Z';
 
 export function bootstrapGalaxyCue(){
